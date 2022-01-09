708399cda9f681122a47fe5c94e095d70278a18c
YourDad07 <salmanadhikari415@gmail.com>
2022-01-09 09:49:03 +0530

    Added The Heroku PORT

--- server.go
+++ server.go
@@ -6,18 +6,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	// err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 	fmt.Println("http://localhost:5000/")
 	router := mux.NewRouter().StrictSlash(true)
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
@@ -31,6 +31,6 @@ func main() {
 	router.HandleFunc("/dashboard", auth.IsAuth(public.DashboardPost)).Methods("POST")
 	router.HandleFunc("/logout", auth.IsAuth(public.Logout)).Methods("GET")
 	router.HandleFunc("/download/{bookpath}", public.Download).Methods("GET")
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 
 }
