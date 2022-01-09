fb22be9cda24c7b005be67111c279bfefe312dd2
YourDad07 <salmanadhikari415@gmail.com>
2022-01-09 10:56:42 +0530

    Retrive The semnumber universityname and branch from table

--- public/home.go
+++ public/home.go
@@ -16,23 +16,23 @@ type tempStrcut struct {
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	db := models.SetupDB()
-	query := "SELECT bookpath,imgpath,subjectname FROM bookinfo"
+	query := "SELECT bookpath,imgpath,subjectname,semester,universityname,branch FROM bookinfo"
 	rows, err := db.Query(query)
 	if err != nil {
 		fmt.Println("Error in getting bookinfo", err)
 	}
-	bookinfo := []tempStrcut{}
+	bookinfo := []models.Book{}
 	defer rows.Close()
-	var book tempStrcut
+	var book models.Book
 	for rows.Next() {
 		fmt.Println("book ")
 
-		err = rows.Scan(&book.Location, &book.Image, &book.Name)
+		err = rows.Scan(&book.Bookfile, &book.Bannerimage, &book.Subjectname, &book.Semnumber, &book.Universityname, &book.Branch)
 		if err != nil {
 			fmt.Println("Error in scanning bookinfo", err)
 		}
-		book.Image = strings.Replace(book.Image, "\\", "/", -1)
-		book.Location = strings.Replace(book.Location, "\\", "/", -1)
+		book.Bannerimage = strings.Replace(book.Bannerimage, "\\", "/", -1)
+		book.Bookfile = strings.Replace(book.Bookfile, "\\", "/", -1)
 		// book.Location = book.Location[60:]
 		// book.Image = book.Image[40:]
 		bookinfo = append(bookinfo, book)
--- server.go
+++ server.go
@@ -13,6 +13,11 @@ import (
 )
 
 func main() {
+	// err := godotenv.Load(".env")
+
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 	fmt.Println("http://localhost:5000/")
 	router := mux.NewRouter().StrictSlash(true)
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
