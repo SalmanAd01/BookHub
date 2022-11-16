c113a2311add24f7a12bc4a4bf1e6a46062ec191
SalmanAd01 <salmanadhikari415@gmail.com>
2022-11-16 23:36:34 +0530

    fix schema

--- seed/createSchema.sql
+++ seed/createSchema.sql
@@ -14,9 +14,10 @@ CREATE TABLE IF NOT EXISTS bookinfo (
     bookpath VARCHAR(255) NOT NULL,
     imgpath VARCHAR(255) NOT NULL,
     subjectname VARCHAR(255) NOT NULL,
-    bookauthor VARCHAR(255) NOT NULL userinfo(id),
+    bookauthor VARCHAR(255) NOT NULL,
     semester VARCHAR(255) NOT NULL,
     branch VARCHAR(255) NOT NULL,
     universityname VARCHAR(255) NOT NULL,
-    userid FOREIGN KEY REFERENCES userinfo(id)
+    userid INTEGER NOT NULL,
+    FOREIGN KEY (userid) REFERENCES userinfo(id)
 );
